041fac7712366a453cf87549f31279133069cc39
Ultra-pn4rv <tsarultra@gmail.com>
2024-04-01 10:14:34 -0400

    questao3

---
+++ questao3.js
@@ -0,0 +1,10 @@
+/*
+Questão 3: Como você pode usar uma declaração if para verificar se uma 
+variável é do tipo string em JavaScript?
+*/
+var mensagem = "Olá mundo"
+if (typeof mensagem === 'string') {
+    console.log('é uma string')
+} else {
+    console.log("n é uma string")
+}
