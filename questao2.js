6fffaf8fc8c7dd6e45a8fdd14b5a219261e699b4
Ultra-pn4rv <tsarultra@gmail.com>
2024-04-01 10:14:30 -0400

    questao2

---
+++ questao2.js
@@ -0,0 +1,10 @@
+/*
+Questão 2: Como você pode usar uma declaração if para verificar se uma 
+string está vazia em JavaScript?
+*/
+let preenchida = 1
+if (preenchida > 0) {
+    console.log("preenchida")
+} else {
+    console.log("vazia")
+}
